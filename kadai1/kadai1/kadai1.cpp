32e402a5afeb5498bc5d597563406c545f4edc35
Yuya-1130 <ktc25a31a0059@edu.kyoto-tech.ac.jp>
2026-06-27 13:16:36 +0900

    kadai1

---
+++ kadai1/kadai1/kadai1.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+class Animal {
+public:
+   virtual void call() {
+        std::cout << "鳴き声" << std::endl;
+    }
+
+};
+class Dog : public Animal {
+
+public:
+
+    void call() override {
+        std::cout << "ワン" << std::endl;
+    }
+};
+
+class Cat : public Animal {
+
+public:
+
+    void call() override {
+        std::cout << "にゃん" << std::endl;
+    }
+};
+
+int main()
+{
+    Animal* animals[] = {new Dog,new Cat};
+
+    for (int i = 0; i < 2; ++i) {
+        animals[i]->call();
+    }
+    for (int i = 0; i < 2; ++i) {
+        delete animals[i];
+        animals[i] = nullptr;
+    }
+
+}
